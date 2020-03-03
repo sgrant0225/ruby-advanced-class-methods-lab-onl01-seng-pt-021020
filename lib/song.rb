0e2a4cd40e9ba59cd8b60012a9ccefffb56e58d7
Sharnell Grant <sharnellgrant@gmail.com>
2020-03-03 00:37:53 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -6,8 +6,19 @@ class Song
     @@all
   end
 
-  def save
+  def save #instance method  
     self.class.all << self
   end
 
+def self.create
+   song_obj = self.new #this creates a new song and set it equal to a variable
+   song_obj.save #any new song you create you want to save it 
+   return song_obj
+ end
+ 
+ def self.new_by_name(song_name)
+   song_obj = self.new 
+   song_obj.name = song_name
+   return song_obj
+ end
 end
